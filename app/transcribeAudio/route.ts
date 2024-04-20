ba70bb5c9e4d9d428063bd8a72776d11f7a70310
Ramesh Aryal <ramesh.aryal@socoro.com.au>
2024-04-20 20:12:04 +1000

    fixed the build error

--- app/transcribeAudio/route.ts
+++ app/transcribeAudio/route.ts
@@ -7,24 +7,22 @@ export async function POST(request: NextRequest) {
     const file = formData.get('audio') as File;
     console.log(">>>>>", file);
 
-     if (
-    process.env.AZURE_API_KEY === undefined ||
-    process.env.AZURE_ENDPOINT === undefined ||
-    process.env.AZURE_DEPLOYMENT_NAME === undefined
-  ) {
-    console.error("Azure credentials not set");
-    return {
-      sender: "",
-      response: "Azure credentials not set",
-    };
-     }
+    if (
+        process.env.AZURE_API_KEY === undefined ||
+        process.env.AZURE_ENDPOINT === undefined ||
+        process.env.AZURE_DEPLOYMENT_NAME === undefined
+    ) {
+        console.error("Azure credentials not set");
+        return NextResponse.json({
+        error: "Azure credentials not set",
+        });
+    }
     
     
     if (file.size === 0) {
-        return {
-            sender: "",
-            response: "No audio file provided"
-        }
+        return NextResponse.json({
+            error: "No audio file provided",
+        });
     }
 
     const arrayBuffer = await file.arrayBuffer();
